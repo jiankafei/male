0c71177312aae1e213ada90b461ad5bb387edd1e
zr <Wll001992Yx@163.com>
2019-02-18 15:00:25 +0800

    temp

--- frame/runtime.js
+++ frame/runtime.js
@@ -2,14 +2,13 @@ import regeneratorRuntime from './regenerator';
 App.regeneratorRuntime = regeneratorRuntime;
 
 // Promise.prototype.finally
-if (typeof Promise.prototype.finally === 'function') {
-  return;
+if (!Promise.prototype.finally) {
+  Reflect.defineProperty(Promise.prototype, 'finally', {
+    enumerable: true,
+    value(callback) {
+      return this
+        .then(res => this.constructor.resolve(callback()).then(() => res))
+        .catch(err => this.constructor.resolve(callback()).then(() => { throw err }));
+    },
+  });
 }
-Reflect.defineProperty(Promise.prototype, 'finally', {
-  enumerable: true,
-  value(callback) {
-    return this
-      .then(res => this.constructor.resolve(callback()).then(() => res))
-      .catch(err => this.constructor.resolve(callback()).then(() => { throw err }));
-  },
-});
