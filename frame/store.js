ddc8afe717e1162948fb7313c528dcdf70462880
zr <Wll001992Yx@163.com>
2019-03-21 15:57:06 +0800

    temp

--- frame/store.js
+++ frame/store.js
@@ -4,21 +4,21 @@ import {
 
 // 计算导航栏布局
 const getNavBarInfo = function(systemInfo) {
-  const offset = /ios/i.test(systemInfo.system) ? 7 : 9;
+  const offset = /ios/i.test(systemInfo.system) ? 6 : 8;
   const statusBarHeight = systemInfo.statusBarHeight;
   const menuWidth = 87;
-  const menuHeight = 30;
+  const menuHeight = 32;
   const menuTop = statusBarHeight + offset;
-  const menuRight = 12;
+  const menuRight = 10;
   const navBarHeight = menuHeight + menuTop + offset;
   const navBarPadding = `${menuTop}px ${menuRight}px ${offset}px`;
   return {
+    statusBarHeight,
+    navBarHeight,
     menuWidth,
     menuHeight,
     menuTop,
     menuRight,
-    statusBarHeight,
-    navBarHeight,
     navBarPadding,
     offset, // statusBar 和 menu 的间隔距离
   };
